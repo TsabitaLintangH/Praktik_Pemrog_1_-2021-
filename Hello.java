68854ba65422337c6f31e92d1e87ccdbc37bddf9
TsabitaLintangH <tsabitalintang.2021@student.uny.ac.id>
2021-09-19 08:12:27 +0700

    menambahkan komentar berupa nama dan nim

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM : 21537144018
+Nama : Tsabita Lintang H
+*/
 public class Hello {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        // untuk menambah teks 
         System.out.println("Hello world!");
     }
 }
