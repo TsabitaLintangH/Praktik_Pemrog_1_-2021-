c80e9382ff77a0080b428b400f9e24c59613d145
TsabitaLintangH <tsabitalintang.2021@student.uny.ac.id>
2021-09-19 12:20:52 +0700

    Tugas Pertemuan 3

---
+++ Pertemuan 3/identitasdiri.java
@@ -0,0 +1,18 @@
+public class identitasdiri {
+    public static void main(String[] args) {
+        String namaLengkap = "Tsabita Lintang Hayuningtias" ;
+        String nim = "21537144018";
+        String angkatan = "2021";
+        String programstudi = "teknologi informasi";
+        String jurusan = "pendidikan teknik elektronika dan informatika";
+        String fakultas = "teknik";
+        double ipkharapan = 3.8;
+        System.out.println("Nama saya " + namaLengkap);
+        System.out.println("NIM saya " + nim);
+        System.out.println("Saya mahasiswa angkatan " + angkatan);
+        System.out.println("Saya dari program studi " + programstudi);
+        System.out.println("Dari jurusan " + jurusan);
+        System.out.println("Fakultas " + fakultas);
+        System.out.println("IPK harapan saya " + ipkharapan);
+    }
+}
---
+++ Pertemuan 3/identitasdiri.py
@@ -0,0 +1,21 @@
+namaLengkap = "Tsabita Lintang Hayuningtias"
+nim = 21537144018
+angkatan = 2021
+programStudi = "Pendidikan Teknologi Elektronika dan informatika"
+jurusan = "Teknologi Informasi"
+fakultas = "Teknik"
+ipkHarapan = 3.8
+print("nama lengkap saya " + namaLengkap)
+print("nim saya " + str(nim))
+print("saya mahasiswa angkatan " + str(angkatan))
+print("saya dari program studi " + programStudi)
+print("saya dari jurusan " + jurusan)
+print("fakultas " + fakultas)
+print("ipk harapan saya " + str(ipkHarapan))
+print(type(namaLengkap))
+print(type(nim))
+print(type(angkatan))
+print(type(programStudi))
+print(type(jurusan))
+print(type(fakultas))
+print(type(ipkHarapan))
