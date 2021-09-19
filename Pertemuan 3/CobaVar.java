84753b84b6bdb1c5ecea7bf1e728ad53b04f8c24
TsabitaLintangH <tsabitalintang.2021@student.uny.ac.id>
2021-09-19 09:37:31 +0700

    belajar variable dalam java

---
+++ Pertemuan 3/CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaDepan = "Tsabita" ;
+        int umur = 18;
+        double tinggiBadan = 156;
+        System.out.println("Nama saya " + namaDepan);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+    }
+}
