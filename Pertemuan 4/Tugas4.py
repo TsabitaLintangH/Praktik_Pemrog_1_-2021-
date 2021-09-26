cec86bf1316ee47745603a331a8167a6fcd1be71
TsabitaLintangH <tsabitalintang.2021@student.uny.ac.id>
2021-09-26 17:09:51 +0700

    balajar operator

---
+++ Pertemuan 4/Tugas4.py
@@ -0,0 +1,19 @@
+a = 45
+b = 15
+hasil = a + b
+print("hasil = " + str(hasil))
+
+a = 45
+b = 15
+hasil = a - b
+print("hasil = " + str(hasil))
+
+a = 45
+b = 15
+hasil = a * b
+print("hasil = " + str(hasil))
+
+a = 45
+b = 15
+hasil = a / b 
+print("hasil = " + str(hasil))
